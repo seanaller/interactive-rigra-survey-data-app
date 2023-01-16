c5eb7a34a8a95719b7d920696c9aaecad2985c3b
seanaller <seandavid.a@gmail.com>
2023-01-16 21:49:29 +0000

    Refactor app to shift to open api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,51 @@
 # streamlit_app.py
 
+## Environment Setup
+import pandas as pd
+import gspread
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
+import plotly.express as px
+import plotly.graph_objects as go
+
+## Load Google Sheet
+gc = gspread.service_account('.streamlit/credentials.json')
+gc_responses = gc.open_by_key(st.secrets.gsheet.sheet_id)
+df_responses = pd.DataFrame(gc_responses.worksheet(st.secrets.gsheet.sheet_responses).get_all_records())
+
+## App Setup
+st.set_page_config(
+    page_title="rigra", page_icon="🖼️", initial_sidebar_state="collapsed"
 )
-conn = connect(credentials=credentials)
+st.markdown("# RIGRA Winter Survey 2022")
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+## Survey Responses Bar Chat - All
+st.bar_chart(df_responses[df_responses['Type'] == 'all'],
+             x = 'Description',
+             y = 'Number')
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+## Survey Responses Bar Chat - Resident Type
+df_responses_resident_type = df_responses[df_responses['Type'] == 'resident_type']
+pie_counts_resident_type = go.Figure(
+    data = [
+        go.Pie(labels = df_responses_resident_type.Description,
+               values = df_responses_resident_type.Number,
+               hole = .3)
+        ]
+    )
+st.plotly_chart(pie_counts_resident_type, use_container_width=True)
 
-# Print results
-for row in rows:
-    st.write(f"{row.Description}: {row.Type} = {number}")
+## Survey Responses Bar Chat - Building Name
+st.bar_chart(df_responses[df_responses['Type'] == 'building_name'],
+             x = 'Description',
+             y = 'Number')
+
+## Survey Responses Bar Chat - Building Floor
+st.bar_chart(df_responses[df_responses['Type'] == 'building_floor'],
+             x = 'Description',
+             y = 'Number')
+
+## Survey Responses Bar Chat - Resident Length
+st.bar_chart(df_responses[df_responses['Type'] == 'resident_length'],
+             x = 'Description',
+             y = 'Number')
